e4069ba6c4859561d8b92a637af99c42d43738c7
farjadakbar4 <farjadakbar4@gmail.com>
2025-04-19 21:13:16 +0500

    fix: pacakge

--- database/firebase.js
+++ database/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-native';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 var firebaseConfig = {
